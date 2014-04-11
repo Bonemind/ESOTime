431999c5ac03235af434e1ba2e4fea9f0ceb7e49
Bonemind <Subhime@gmail.com>
2014-04-12 00:03:44 +0200

    Added toggleable FPS counter

--- ESOTime.lua
+++ ESOTime.lua
@@ -1,6 +1,14 @@
-local counter = 1
+ZO_CreateStringId("SI_BINDING_NAME_TOGGLE_FPS", "Toggle FPS")
+local showFPS = false
 function ESOTimeUpdate()
-	ESOTimeCounter:SetText(" " .. GetTimeString())
+	text = " " .. GetTimeString()
+	if showFPS == true then
+		text = text  .. "   FPS:" .. tostring(math.floor(GetFramerate()))
+	end
+	ESOTimeCounter:SetText(text)
 	counter = counter + 1
 end
 
+function ToggleFps()
+	showFPS = (not showFPS)
+end
